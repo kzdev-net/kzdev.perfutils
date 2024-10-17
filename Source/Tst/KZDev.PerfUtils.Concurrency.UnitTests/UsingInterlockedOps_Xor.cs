d1ab214613599d078e07e1c84eb1efc726c78181
Kevin Zehrer <103638980+kzdev-net@users.noreply.github.com>
2024-10-17 16:25:01 -0500

    Add ExcludeFromCodeCoverage attribute to UsingInterlockedOps
    Applied ExcludeFromCodeCoverage attribute to UsingInterlockedOps
    class to exclude it from code coverage analysis.

--- Source/Tst/KZDev.PerfUtils.Concurrency.UnitTests/UsingInterlockedOps_Xor.cs
+++ Source/Tst/KZDev.PerfUtils.Concurrency.UnitTests/UsingInterlockedOps_Xor.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Kevin Zehrer
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics.CodeAnalysis;
+
 using FluentAssertions;
 
 namespace KZDev.PerfUtils.Tests
@@ -10,6 +12,7 @@ namespace KZDev.PerfUtils.Tests
     /// Unit tests for the <see cref="InterlockedOps"/> Xor operations.
     /// </summary>
     [Trait(TestConstants.TestTrait.Category, TestConstants.TestCategory.Concurrency)]
+    [ExcludeFromCodeCoverage]
     public partial class UsingInterlockedOps : UnitTestBase
     {
         #region Xor Tests
