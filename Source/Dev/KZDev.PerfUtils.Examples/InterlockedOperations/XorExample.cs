ce8fb230ff1fee58d3c812968c9ee90baf9b9da3
Kevin Zehrer <103638980+kzdev-net@users.noreply.github.com>
2024-10-24 17:10:53 -0500

    Updated the interlocked operations documentation.

---
+++ Source/Dev/KZDev.PerfUtils.Examples/InterlockedOperations/XorExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KZDev.PerfUtils;
+
+namespace KZDev.PerfUtils.Examples
+{
+    public class XorExample
+    {
+        private int _flag;
+
+        public bool ToggleFlag ()
+        {
+            int originalValue = InterlockedOps.Xor(ref _flag, 1);
+            return originalValue == 0;
+        }
+    }
+}
