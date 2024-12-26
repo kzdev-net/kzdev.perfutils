87c33989c6a63e6326cfae8a15ce4d092a8f8e46
Kevin Zehrer <103638980+kzdev-net@users.noreply.github.com>
2024-12-25 21:40:59 -0600

    Reduce test loop iterations to improve test execution time
    The number of iterations in various test loops within the
    `UsingMemorySegmentedBufferGroupWithUsedPatterns.cs` file has been
    reduced from 1000 to 200. This change affects multiple test methods,
    including:
    - `UsingMemorySegmentedBufferGroup_GetRemainingBuffers_WithRandomUsedPatterns_GetsProperBuffers`
    - `UsingMemorySegmentedBufferGroup_GetRemainingBuffers_WithRandomUsedBlockFlags_GetsProperBuffers`
    - `UsingMemorySegmentedBufferGroup_GetPreferredRemainingBuffers_WithRandomUsedPatterns_GetsProperBuffers`
    - `UsingMemorySegmentedBufferGroup_GetPreferredRemainingBuffers_WithRandomUsedBlockFlags_GetsProperBuffers`
    - `UsingMemorySegmentedBufferGroup_GetRandomBuffers_WithRandomUsedBlockFlags_GetsProperBuffers`
    - `UsingMemorySegmentedBufferGroup_GetRandomBuffers_WithRandomUsedPatterns_GetsProperBuffers`
    
    In the `UsingMemoryStreamSlim.ReadWrite.cs` file, the number of test
    loops in the `UsingMemoryStreamSlim_GetArray_ReturnsCorrectData` method
    has been fixed at 50, instead of using a random number of test loops
    between `MinimumTestLoops` and `MaximumTestLoops`.
    
    These changes aim to decrease the execution time of the tests.

--- Source/Tst/Shared/KZDev.PerfUtils.MemoryStreamSlim.Tests.Shared/UsingMemoryStreamSlim.ReadWrite.cs
+++ Source/Tst/Shared/KZDev.PerfUtils.MemoryStreamSlim.Tests.Shared/UsingMemoryStreamSlim.ReadWrite.cs
@@ -393,10 +393,9 @@ namespace KZDev.PerfUtils.Tests
         public async Task UsingMemoryStreamSlim_GetArray_ReturnsCorrectData ()
         {
             int[] testDataSizes = GenerateTestDataSizes(5000, 0xF_FFFF).ToArray();
-            int testLoops = RandomSource.GetRandomInteger(MinimumTestLoops, MaximumTestLoops + 1);
 
             foreach (int testSegmentSize in TestSegmentSizes)
-                for (int testLoop = 0; testLoop < testLoops; testLoop++)
+                for (int testLoop = 0; testLoop < 50; testLoop++)
                 {
                     int byteCount = testDataSizes[RandomSource.GetRandomInteger(testDataSizes.Length)];
                     await using MemoryStreamSlim testService = CreateTestService(byteCount);
