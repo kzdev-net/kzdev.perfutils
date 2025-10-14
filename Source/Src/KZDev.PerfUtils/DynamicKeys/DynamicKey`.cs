1e43d0b15b4aa784fc78d712ab1ddad34fb1dd7b
Kevin Zehrer <103638980+kzdev-net@users.noreply.github.com>
2025-10-14 05:24:05 -0500

    Updated DynamicKey<T> to use Lambda expressions for the most efficient key creation.

--- Source/Src/KZDev.PerfUtils/DynamicKeys/DynamicKey`.cs
+++ Source/Src/KZDev.PerfUtils/DynamicKeys/DynamicKey`.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) Kevin Zehrer
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
+using System.Linq.Expressions;
+using System.Reflection;
+
 namespace KZDev.PerfUtils;
 
 //################################################################################
@@ -14,6 +17,130 @@ namespace KZDev.PerfUtils;
 /// </remarks>
 public static class DynamicKey<T>
 {
+    /// <summary>
+    /// Cached delegate for efficient key creation without boxing.
+    /// This factory is created once per type T using reflection and compiled lambda expressions,
+    /// then reused for all subsequent calls to avoid both boxing and reflection overhead.
+    /// </summary>
+    private static readonly Func<T, DynamicKey> _keyFactory = CreateKeyFactory();
+
+    //--------------------------------------------------------------------------------
+    /// <summary>
+    /// Creates a compiled lambda expression for efficient key creation based on the type T.
+    /// </summary>
+    /// <returns>A compiled delegate that creates the appropriate DynamicKey without boxing.</returns>
+    private static Func<T, DynamicKey> CreateKeyFactory ()
+    {
+        Type type = typeof(T);
+
+        // Handle special cases first
+        if (type == typeof(int))
+            return (Func<T, DynamicKey>)(object)new Func<int, DynamicKey>(DynamicIntKey.GetKey);
+        if (type == typeof(long))
+            return (Func<T, DynamicKey>)(object)new Func<long, DynamicKey>(DynamicLongKey.GetKey);
+        if (type == typeof(uint))
+            return (Func<T, DynamicKey>)(object)new Func<uint, DynamicKey>(DynamicUIntKey.GetKey);
+        if (type == typeof(ulong))
+            return (Func<T, DynamicKey>)(object)new Func<ulong, DynamicKey>(DynamicULongKey.GetKey);
+        if (type == typeof(bool))
+            return (Func<T, DynamicKey>)(object)new Func<bool, DynamicKey>(DynamicBoolKey.GetKey);
+        if (type == typeof(string))
+            return (Func<T, DynamicKey>)(object)new Func<string, DynamicKey>(DynamicStringKey.GetKey);
+        if (type == typeof(Guid))
+            return (Func<T, DynamicKey>)(object)new Func<Guid, DynamicKey>(DynamicGuidKey.GetKey);
+        if (type == typeof(Type))
+            return (Func<T, DynamicKey>)(object)new Func<Type, DynamicKey>(DynamicTypeKey.GetKey);
+
+        // Handle enums
+        if (type.IsEnum)
+        {
+            return CreateEnumKeyFactory(type);
+        }
+
+        // Handle other value types
+        if (type.IsValueType)
+        {
+            return CreateValueTypeKeyFactory(type);
+        }
+
+        // Handle reference types
+        return CreateReferenceTypeKeyFactory(type);
+    }
+    //--------------------------------------------------------------------------------
+    /// <summary>
+    /// Creates a factory for enum types using DynamicEnumKey.
+    /// </summary>
+    private static Func<T, DynamicKey> CreateEnumKeyFactory (Type enumType)
+    {
+        try
+        {
+            // Get the generic DynamicEnumKey.GetKey method
+            MethodInfo getKeyMethod = typeof(DynamicEnumKey<>)
+                .MakeGenericType(enumType)
+                .GetMethod(nameof(DynamicRefKey<object>.GetKey), BindingFlags.Public | BindingFlags.Static)!;
+
+            // Create lambda: value => DynamicEnumKey<TEnum>.GetKey(value)
+            ParameterExpression valueParam = Expression.Parameter(typeof(T), "value");
+            MethodCallExpression call = Expression.Call(getKeyMethod, valueParam);
+
+            return Expression.Lambda<Func<T, DynamicKey>>(call, valueParam).Compile();
+        }
+        catch
+        {
+            // Fallback to DynamicObjectKey if reflection fails
+            return value => DynamicObjectKey.GetKey(value!);
+        }
+    }
+    //--------------------------------------------------------------------------------
+    /// <summary>
+    /// Creates a factory for value types using DynamicValKey.
+    /// </summary>
+    private static Func<T, DynamicKey> CreateValueTypeKeyFactory (Type valueType)
+    {
+        try
+        {
+            // Get the generic DynamicValKey.GetKey method
+            MethodInfo getKeyMethod = typeof(DynamicValKey<>)
+                .MakeGenericType(valueType)
+                .GetMethod(nameof(DynamicRefKey<object>.GetKey), BindingFlags.Public | BindingFlags.Static)!;
+
+            // Create lambda: value => DynamicValKey<T>.GetKey(value)
+            ParameterExpression valueParam = Expression.Parameter(typeof(T), "value");
+            MethodCallExpression call = Expression.Call(getKeyMethod, valueParam);
+
+            return Expression.Lambda<Func<T, DynamicKey>>(call, valueParam).Compile();
+        }
+        catch
+        {
+            // Fallback to DynamicObjectKey if reflection fails
+            return value => DynamicObjectKey.GetKey(value!);
+        }
+    }
+    //--------------------------------------------------------------------------------
+    /// <summary>
+    /// Creates a factory for reference types using DynamicRefKey.
+    /// </summary>
+    private static Func<T, DynamicKey> CreateReferenceTypeKeyFactory (Type referenceType)
+    {
+        try
+        {
+            // Get the generic DynamicRefKey.GetKey method
+            MethodInfo getKeyMethod = typeof(DynamicRefKey<>)
+                .MakeGenericType(referenceType)
+                .GetMethod(nameof(DynamicRefKey<object>.GetKey), BindingFlags.Public | BindingFlags.Static)!;
+
+            // Create lambda: value => DynamicRefKey<T>.GetKey(value)
+            ParameterExpression valueParam = Expression.Parameter(typeof(T), "value");
+            MethodCallExpression call = Expression.Call(getKeyMethod, valueParam);
+
+            return Expression.Lambda<Func<T, DynamicKey>>(call, valueParam).Compile();
+        }
+        catch
+        {
+            // Fallback to DynamicObjectKey if reflection fails
+            return value => DynamicObjectKey.GetKey(value!);
+        }
+    }
     //--------------------------------------------------------------------------------
     /// <summary>
     /// Gets a <see cref="DynamicKey"/> instance for the given value.
@@ -31,35 +158,8 @@ public static class DynamicKey<T>
             return DynamicKey.Null;
         if (value is DynamicKey dynamicKey)
             return dynamicKey;
-        switch (value)
-        {
-            case int intValue:
-                return DynamicIntKey.GetKey (intValue);
 
-            case long longValue:
-                return DynamicLongKey.GetKey (longValue);
-
-            case uint uintValue:
-                return DynamicUIntKey.GetKey (uintValue);
-
-            case ulong ulongValue:
-                return DynamicULongKey.GetKey (ulongValue);
-
-            case bool boolValue:
-                return DynamicBoolKey.GetKey (boolValue);
-
-            case string stringValue:
-                return DynamicStringKey.GetKey (stringValue);
-
-            case Guid guidValue:
-                return DynamicGuidKey.GetKey (guidValue);
-
-            case Type typeValue:
-                return DynamicTypeKey.GetKey (typeValue);
-
-            default:
-                return DynamicObjectKey.GetKey (value);
-        }
+        return _keyFactory(value);
     }
     //--------------------------------------------------------------------------------
 }
